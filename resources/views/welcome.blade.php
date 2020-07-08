ee29d772b0b56457b9d2e8a837b22018346f1975
Liar <1930804855@qq.com>
2020-07-08 19:10:08 +0800

    首页欢迎语

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    1911微信 Hello Word!
                 </div>
 
                 <div class="links">
